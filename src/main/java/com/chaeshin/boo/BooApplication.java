01486624cc0ee97253dbd4b3cf086e6089ac62ab
ShinSub98 <wjdtjq26@naver.com>
2024-07-21 03:08:17 +0900

    <build>:settings for deployment

--- src/main/java/com/chaeshin/boo/BooApplication.java
+++ src/main/java/com/chaeshin/boo/BooApplication.java
@@ -1,9 +1,12 @@
 package com.chaeshin.boo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -13,4 +16,8 @@ public class BooApplication {
         SpringApplication.run(BooApplication.class, args);
     }
 
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
